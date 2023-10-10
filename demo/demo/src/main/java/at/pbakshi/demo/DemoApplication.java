2a2960e512e10ef077c3dffc0c3cb8a9ea847df9
pbakshi05 <117721905+pbakshi05@users.noreply.github.com>
2023-10-10 16:40:29 +0200

    Add files via upload
    Code

---
+++ demo/demo/src/main/java/at/pbakshi/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package at.pbakshi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String test(){
+		return "Ich fick Sebastians Mutter!";
+	}
+
+}
